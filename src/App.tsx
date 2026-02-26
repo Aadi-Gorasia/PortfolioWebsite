5660e6f4db1316c1f243a2ba77bb1c8c0df51ab7
Gorasia . <aadigorasia6@gmail.com>
2026-02-26 16:41:27 +0530

    Updated Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -104,7 +104,7 @@ export default function App() {
           <AboutSection />
           <ExpertiseSection />
 <SelectedWorks />  
-<IdentitySection />
+<TheArsenal />
 <EditorialSection />
 <Contact /> 
 <Footer />
