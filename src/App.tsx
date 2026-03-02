8e17618f8a5a2aeeb23be362e77ecf2e43db2492
Gorasia . <aadigorasia6@gmail.com>
2026-03-02 20:49:04 +0530

    Fixed Routes Duplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
